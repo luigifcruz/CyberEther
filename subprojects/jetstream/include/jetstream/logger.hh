426f84aff1f556f34a22414f40d38f758d61975e
luigifcruz <luigifcruz@gmail.com>
2022-04-25 21:01:57 -0700

    Update logger.

--- subprojects/jetstream/include/jetstream/logger.hh
+++ subprojects/jetstream/include/jetstream/logger.hh
@@ -11,31 +11,43 @@
 #define JST_LOG_HEAD_DECR fmt::emphasis::bold
 #define JST_LOG_HEAD_NAME fmt::format(JST_LOG_HEAD_DECR, "JETSTREAM ")
 #define JST_LOG_HEAD_FILE fmt::format(JST_LOG_HEAD_DECR, "[{}@{}] ", __FILENAME__, __LINE__)
+#define JST_LOG_HEAD_TRACE fmt::format(JST_LOG_HEAD_DECR, "[TRACE] ")
+#define JST_LOG_HEAD_DEBUG fmt::format(JST_LOG_HEAD_DECR, "[DEBUG] ")
+#define JST_LOG_HEAD_WARN fmt::format(JST_LOG_HEAD_DECR, "[WARN]  ")
+#define JST_LOG_HEAD_INFO fmt::format(JST_LOG_HEAD_DECR, "[INFO]  ")
+#define JST_LOG_HEAD_ERROR fmt::format(JST_LOG_HEAD_DECR, "[ERROR] ")
+#define JST_LOG_HEAD_FATAL fmt::format(JST_LOG_HEAD_DECR, "[FATAL] ")
+
 #define JST_LOG_HEAD_SEPR fmt::format(JST_LOG_HEAD_DECR, "| ")
-#define JST_LOG_HEAD JST_LOG_HEAD_NAME << JST_LOG_HEAD_FILE << JST_LOG_HEAD_SEPR 
 
 #if !defined(JST_TRACE) || !defined(NDEBUG)
-#define JST_TRACE(...) std::cout << JST_LOG_HEAD << fmt::format(fg(fmt::color::white), __VA_ARGS__) << std::endl;
+#define JST_TRACE(...) std::cout << JST_LOG_HEAD_NAME << JST_LOG_HEAD_FILE << JST_LOG_HEAD_TRACE << \
+        JST_LOG_HEAD_SEPR << fmt::format(fg(fmt::color::white), __VA_ARGS__) << std::endl;
 #endif
 
-#if !defined(JST_DEBUG) || !defined(NDEBUG)
-#define JST_DEBUG(...) std::cout << JST_LOG_HEAD << fmt::format(fg(fmt::color::orange), __VA_ARGS__) << std::endl;
+#if !defined(JST_DEBUG) || defined(NDEBUG)
+#define JST_DEBUG(...) std::cout << JST_LOG_HEAD_NAME << JST_LOG_HEAD_DEBUG << JST_LOG_HEAD_SEPR << \
+        fmt::format(fg(fmt::color::orange), __VA_ARGS__) << std::endl;
 #endif
 
 #if !defined(JST_WARN) || defined(NDEBUG)
-#define JST_WARN(...) std::cout << JST_LOG_HEAD << fmt::format(fg(fmt::color::yellow), __VA_ARGS__) << std::endl;
+#define JST_WARN(...) std::cout << JST_LOG_HEAD_NAME << JST_LOG_HEAD_WARN << JST_LOG_HEAD_SEPR << \
+        fmt::format(fg(fmt::color::yellow), __VA_ARGS__) << std::endl;
 #endif
 
 #if !defined(JST_INFO) || defined(NDEBUG)
-#define JST_INFO(...) std::cout << JST_LOG_HEAD << fmt::format(fg(fmt::color::cyan), __VA_ARGS__) << std::endl;
+#define JST_INFO(...) std::cout << JST_LOG_HEAD_NAME << JST_LOG_HEAD_INFO << JST_LOG_HEAD_SEPR << \
+        fmt::format(fg(fmt::color::cyan), __VA_ARGS__) << std::endl;
 #endif
 
 #if !defined(JST_ERROR) || defined(NDEBUG)
-#define JST_ERROR(...) std::cerr << JST_LOG_HEAD << fmt::format(fg(fmt::color::red), __VA_ARGS__) << std::endl;
+#define JST_ERROR(...) std::cerr << JST_LOG_HEAD_NAME << JST_LOG_HEAD_FILE << JST_LOG_HEAD_ERROR << \
+        JST_LOG_HEAD_SEPR << fmt::format(fg(fmt::color::red), __VA_ARGS__) << std::endl;
 #endif
 
 #if !defined(JST_FATAL) || defined(NDEBUG)
-#define JST_FATAL(...) std::cerr << JST_LOG_HEAD << fmt::format(fg(fmt::color::magenta), __VA_ARGS__) << std::endl;
+#define JST_FATAL(...) std::cerr << JST_LOG_HEAD_NAME << JST_LOG_HEAD_FILE << JST_LOG_HEAD_FATAL << \
+        JST_LOG_HEAD_SEPR << fmt::format(fg(fmt::color::magenta), __VA_ARGS__) << std::endl;
 #endif
 
 #endif
