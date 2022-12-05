8d9b39278d0b5c83c125e5bd9eaa3b5bb129d2fe
Luigi F. Cruz <luigifcruz@gmail.com>
2022-12-04 17:38:53 -0800

    Add generic Viewport.

--- subprojects/jetstream/include/jetstream/viewport/base.hh
+++ subprojects/jetstream/include/jetstream/viewport/base.hh
@@ -1,6 +1,8 @@
 #ifndef JETSTREAM_VIEWPORT_BASE_HH
 #define JETSTREAM_VIEWPORT_BASE_HH
 
+#include "jetstream/viewport/generic.hh"
+
 #ifdef JETSTREAM_VIEWPORT_MACOS_AVAILABLE
 #include "jetstream/viewport/macos.hh"
 #endif
