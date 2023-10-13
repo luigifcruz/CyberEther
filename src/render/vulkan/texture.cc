a2967eb90bad6c069a094a70d3b7a6894a7ff21e
Luigi F. Cruz <luigifcruz@gmail.com>
2023-10-13 20:43:25 -0300

    Vulkan fallback to nearest interpolation.

--- src/render/vulkan/texture.cc
+++ src/render/vulkan/texture.cc
@@ -84,10 +84,23 @@ Result Implementation::create() {
 
     // Create sampler.
 
+    VkFormatProperties formatProperties;
+    vkGetPhysicalDeviceFormatProperties(physicalDevice, pixelFormat, &formatProperties);
+
+    VkFilter filter = VK_FILTER_LINEAR;
+    VkSamplerMipmapMode mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+
+    if (!(formatProperties.optimalTilingFeatures & VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT)) {
+        JST_WARN("[VULKAN] The image format does not support linear filtering. Falling back to nearest filtering.");
+
+        filter = VK_FILTER_NEAREST;
+        mipmapMode = VK_SAMPLER_MIPMAP_MODE_NEAREST;
+    }
+
     VkSamplerCreateInfo samplerCreateInfo = {};
     samplerCreateInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
-    samplerCreateInfo.magFilter = VK_FILTER_LINEAR;
-    samplerCreateInfo.minFilter = VK_FILTER_LINEAR;
+    samplerCreateInfo.magFilter = filter;
+    samplerCreateInfo.minFilter = filter;
     samplerCreateInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
     samplerCreateInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
     samplerCreateInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
@@ -97,13 +110,13 @@ Result Implementation::create() {
     samplerCreateInfo.unnormalizedCoordinates = VK_FALSE;
     samplerCreateInfo.compareEnable = VK_FALSE;
     samplerCreateInfo.compareOp = VK_COMPARE_OP_ALWAYS;
-    samplerCreateInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+    samplerCreateInfo.mipmapMode = mipmapMode;
     samplerCreateInfo.mipLodBias = 0.0f;
     samplerCreateInfo.minLod = 0.0f;
     samplerCreateInfo.maxLod = 1.0f;
 
     JST_VK_CHECK(vkCreateSampler(device, &samplerCreateInfo, nullptr, &sampler), [&]{
-        JST_ERROR("[VULKAN] Can't create texture sampler.")     
+        JST_ERROR("[VULKAN] Can't create texture sampler.");
     });
 
     // Register descriptor for ImGui attachment.
