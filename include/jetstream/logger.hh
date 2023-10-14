0c2313509a5220ed1bb7e83f1af531acb914d25c
Luigi F. Cruz <luigifcruz@gmail.com>
2023-10-13 22:45:23 -0300

    Implement logger locking mechanism for multithreading.

--- include/jetstream/logger.hh
+++ include/jetstream/logger.hh
@@ -2,6 +2,7 @@
 #define JETSTREAM_LOGGER_HH
 
 #include <iostream>
+#include <mutex>
 
 #include <fmt/format.h>
 #include <fmt/color.h>
@@ -13,6 +14,8 @@
 std::string& JST_LOG_LAST_WARNING();
 std::string& JST_LOG_LAST_ERROR();
 std::string& JST_LOG_LAST_FATAL();
+
+std::mutex& _JST_LOG_MUTEX();
 
 #define _JST_LOG_SINK          std::cout
 #define _JST_LOG_ENDL          std::endl;
@@ -44,12 +47,13 @@ std::string& JST_LOG_LAST_FATAL();
 #ifndef JST_TRACE
 #ifdef JST_DEBUG_MODE
 #define JST_TRACE(...) if (getenv("TRACE"))           { \
+                       std::lock_guard<std::mutex> lock(_JST_LOG_MUTEX()); \
                        _JST_LOG_SINK                 << \
                        _JST_LOG_NAME                 << \
                        _JST_LOG_TRACE                << \
                        _JST_LOG_SEPR                 << \
                        _JST_LOG_DEFAULT(__VA_ARGS__) << \
-                       _JST_LOG_ENDL                  };
+                       _JST_LOG_ENDL; }
 #else
 #define JST_TRACE(...)
 #endif
@@ -57,60 +61,70 @@ std::string& JST_LOG_LAST_FATAL();
 
 #ifndef JST_DEBUG
 #ifdef JST_DEBUG_MODE
-#define JST_DEBUG(...) _JST_LOG_SINK                << \
+#define JST_DEBUG(...) { \
+                       std::lock_guard<std::mutex> lock(_JST_LOG_MUTEX()); \
+                       _JST_LOG_SINK                << \
                        _JST_LOG_NAME                << \
                        _JST_LOG_DEBUG               << \
                        _JST_LOG_SEPR                << \
                        _JST_LOG_ORANGE(__VA_ARGS__) << \
-                       _JST_LOG_ENDL;
+                       _JST_LOG_ENDL; }
 #else
 #define JST_DEBUG(...)
 #endif
 #endif
 
 #ifndef JST_INFO
-#define JST_INFO(...) _JST_LOG_SINK              << \
+#define JST_INFO(...) { \
+                      std::lock_guard<std::mutex> lock(_JST_LOG_MUTEX()); \
+                      _JST_LOG_SINK              << \
                       _JST_LOG_NAME              << \
                       _JST_LOG_INFO              << \
                       _JST_LOG_SEPR              << \
                       _JST_LOG_CYAN(__VA_ARGS__) << \
-                      _JST_LOG_ENDL;
+                      _JST_LOG_ENDL; }
 #else
 #define JST_INFO(...)
 #endif
 
 #ifndef JST_WARN
-#define JST_WARN(...) JST_LOG_LAST_WARNING() = _JST_LOG_DEFAULT(__VA_ARGS__); \
+#define JST_WARN(...) { \
+                      JST_LOG_LAST_WARNING() = _JST_LOG_DEFAULT(__VA_ARGS__); \
+                      std::lock_guard<std::mutex> lock(_JST_LOG_MUTEX()); \
                       _JST_LOG_SINK                << \
                       _JST_LOG_NAME                << \
                       _JST_LOG_WARN                << \
                       _JST_LOG_SEPR                << \
                       _JST_LOG_YELLOW(__VA_ARGS__) << \
-                      _JST_LOG_ENDL;
+                      _JST_LOG_ENDL; }
 #else
 #define JST_WARN(...)
 #endif
 
 #ifndef JST_ERROR
-#define JST_ERROR(...) JST_LOG_LAST_ERROR() = _JST_LOG_DEFAULT(__VA_ARGS__); \
+#define JST_ERROR(...) { \
+                       JST_LOG_LAST_ERROR() = _JST_LOG_DEFAULT(__VA_ARGS__); \
+                       std::lock_guard<std::mutex> lock(_JST_LOG_MUTEX()); \
                        _JST_LOG_SINK             << \
                        _JST_LOG_NAME             << \
                        _JST_LOG_ERROR            << \
                        _JST_LOG_SEPR             << \
                        _JST_LOG_RED(__VA_ARGS__) << \
-                       _JST_LOG_ENDL;
+                       _JST_LOG_ENDL; }
 #else
 #define JST_ERROR(...)
 #endif
 
 #ifndef JST_FATAL
-#define JST_FATAL(...) JST_LOG_LAST_FATAL() = _JST_LOG_DEFAULT(__VA_ARGS__); \
+#define JST_FATAL(...) { \
+                       JST_LOG_LAST_FATAL() = _JST_LOG_DEFAULT(__VA_ARGS__); \
+                       std::lock_guard<std::mutex> lock(_JST_LOG_MUTEX()); \
                        _JST_LOG_SINK                 << \
                        _JST_LOG_NAME                 << \
                        _JST_LOG_FATAL                << \
                        _JST_LOG_SEPR                 << \
                        _JST_LOG_MAGENTA(__VA_ARGS__) << \
-                       _JST_LOG_ENDL;
+                       _JST_LOG_ENDL; }
 #else
 #define JST_FATAL(...)
 #endif
--- src/logger.cc
+++ src/logger.cc
@@ -1,16 +1,21 @@
 #include "jetstream/logger.hh"
 
 std::string& JST_LOG_LAST_WARNING() {
-    static std::string _JST_LOG_LAST_WARNING;
-    return _JST_LOG_LAST_WARNING;
+    static std::string __JST_LOG_LAST_WARNING;
+    return __JST_LOG_LAST_WARNING;
 }
 
 std::string& JST_LOG_LAST_ERROR() {
-    static std::string _JST_LOG_LAST_ERROR;
-    return _JST_LOG_LAST_ERROR;
+    static std::string __JST_LOG_LAST_ERROR;
+    return __JST_LOG_LAST_ERROR;
 }
 
 std::string& JST_LOG_LAST_FATAL() {
-    static std::string _JST_LOG_LAST_FATAL;
-    return _JST_LOG_LAST_FATAL;
+    static std::string __JST_LOG_LAST_FATAL;
+    return __JST_LOG_LAST_FATAL;
+}
+
+std::mutex& _JST_LOG_MUTEX() {
+    static std::mutex __JST_LOG_MUTEX;
+    return __JST_LOG_MUTEX;
 }
