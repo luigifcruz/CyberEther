942dbfbe95e3d0503a4dc71d3da870ed12f0e0de
luigifcruz <github@luigi.ltd>
2024-02-21 00:29:02 -0300

    Fix module discontiguous input validator logic.

--- include/jetstream/module.hh
+++ include/jetstream/module.hh
@@ -53,7 +53,7 @@ class JETSTREAM_API Module {
             return Result::ERROR;
         }
 
-        if ((taint & Taint::DISCONTIGUOUS) == Taint::DISCONTIGUOUS && !buffer.contiguous()) {
+        if ((taint & Taint::DISCONTIGUOUS) != Taint::DISCONTIGUOUS && !buffer.contiguous()) {
             JST_ERROR("Input is not contiguous during initialization.");
             return Result::ERROR;
         }
