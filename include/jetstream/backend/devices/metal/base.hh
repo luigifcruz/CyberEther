5f2e8d47d2f464f283e89da6ae88e375c5e6ba70
Luigi Cruz <github@luigi.ltd>
2024-02-22 02:02:42 -0300

    Silence Metal binding warnings.

--- include/jetstream/backend/devices/metal/base.hh
+++ include/jetstream/backend/devices/metal/base.hh
@@ -1,7 +1,12 @@
 #ifndef JETSTREAM_BACKEND_DEVICE_METAL_HH
 #define JETSTREAM_BACKEND_DEVICE_METAL_HH
 
+#pragma GCC diagnostic push 
+#pragma GCC diagnostic ignored "-Wgnu-anonymous-struct"
+#pragma GCC diagnostic ignored "-Wnested-anon-types"
+#pragma GCC diagnostic ignored "-Wdtor-name"
 #include "jetstream/backend/devices/metal/bindings.hpp"
+#pragma GCC diagnostic pop
 #include "jetstream/backend/config.hh"
 
 namespace Jetstream::Backend {
