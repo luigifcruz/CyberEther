d7967a1fa35a35735ec5571704364623b1427a5d
luigifcruz <github@luigi.ltd>
2024-03-02 00:27:58 -0300

    Add initial Tensor Storage unit tests.

---
+++ tests/memory/storage.cc
@@ -0,0 +1,46 @@
+#include <sstream>
+
+#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include "jetstream/memory/base.hh"
+
+using namespace Jetstream;
+
+TEST_CASE("Storage Class Tests", "[Storage]") {
+    SECTION("Default Constructor") {
+        Tensor<Device::CPU, F32> storage;
+
+        REQUIRE(storage.root_device() == Device::CPU);
+        REQUIRE(storage.compatible_devices().size() > 1);
+        REQUIRE(storage.compatible_devices().contains(Device::CPU));
+        REQUIRE(storage.references() == 1);
+        REQUIRE(storage.attributes().empty());
+    }
+
+    // TODO: Add more tests.
+
+#if defined(JETSTREAM_BACKEND_VULKAN_AVAILABLE)
+    SECTION("Clone") {
+        Tensor<Device::Vulkan, F32> storage({1}, true);
+
+        REQUIRE(storage.device() == Device::Vulkan);
+        REQUIRE(storage.root_device() == Device::Vulkan);
+        REQUIRE(storage.references() == 1);
+        REQUIRE(storage.compatible_devices().contains(Device::Vulkan));
+        
+
+        Tensor<Device::CPU, F32> cloned_storage(storage);
+
+        REQUIRE(cloned_storage.device() == Device::CPU);
+        REQUIRE(cloned_storage.root_device() == Device::Vulkan);
+        REQUIRE(cloned_storage.references() == 2);
+        REQUIRE(cloned_storage.compatible_devices().contains(Device::Vulkan));
+        REQUIRE(cloned_storage.compatible_devices().contains(Device::CPU));
+    }
+#endif
+}
+
+int main(int argc, char* argv[]) {
+  return Catch::Session().run(argc, argv);
+}
