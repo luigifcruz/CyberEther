fc678782d17849b8a70c5eb5fd31d837d4c161d3
Luigi Cruz <github@luigi.ltd>
2024-09-25 23:13:57 -0400

    Update Naga to latest version.

--- shaders/global/text.vert.glsl
+++ shaders/global/text.vert.glsl
@@ -7,11 +7,17 @@ layout(set = 0, binding = 0) uniform ShaderUniforms {
 
 layout(location = 0) in vec2 inPosition;
 layout(location = 1) in vec2 inTexcoord;
-layout(location = 2) in mat4 inTransform;
+
+// TODO: Patch for WebGPU. Implement Naga support for `mat4` scalars.
+layout(location = 2) in vec4 inTransform0;
+layout(location = 3) in vec4 inTransform1;
+layout(location = 4) in vec4 inTransform2;
+layout(location = 5) in vec4 inTransform3;
 
 layout(location = 0) out vec2 outTexcoord;
 
 void main() {
+    mat4 inTransform = mat4(inTransform0, inTransform1, inTransform2, inTransform3);
     gl_Position = inTransform * vec4(inPosition, 1.0, 1.0);
     outTexcoord = inTexcoord;
 }
