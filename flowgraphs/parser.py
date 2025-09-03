456b30f9bec4edc36f151d53436e2cbf30ecfded
Luigi Cruz <github@luigi.ltd>
2025-09-03 14:21:45 +0200

    Update flowgraph build files.

--- flowgraphs/parser.py
+++ flowgraphs/parser.py
@@ -20,12 +20,12 @@ def yml_to_header(path, output, inputs):
         f.write('#include "jetstream/flowgraph.hh"\n\n')
 
         f.write('namespace Jetstream::Flowgraphs {\n\n')
-        
+
         f.write('inline Result GetDefaultManifest(Flowgraph::MetadataManifest& manifest) {\n')
 
         for file in inputs:
             filename = os.path.basename(file).split('.')[0]
-            
+
             with open(os.path.join(path, file), 'r') as yml_file:
                 if YAML_AVAILABLE:
                     yml_content = yaml.safe_load(yml_file)
@@ -36,10 +36,10 @@ def yml_to_header(path, output, inputs):
                     title = filename
                     summary = "No summary."
                     description = 'No description.'
-                
+
                 yml_file.seek(0)
                 content = yml_file.read().replace('\n', '\\n').replace('"', '\\"')
-                
+
                 f.write(f'    manifest["{filename}"] = {{"{title}", "{summary}", "{description}", "{content}"}};\n')
 
         f.write('    return Result::SUCCESS;\n')
