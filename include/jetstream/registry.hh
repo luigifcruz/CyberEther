58c4d0ccfe04489c6dfbc3b84ab3ffb913d3ee15
Luigi Cruz <github@luigi.ltd>
2026-04-22 12:37:41 -0700

    refactor(registry): add cross-platform static registration helpers

--- include/jetstream/registry.hh
+++ include/jetstream/registry.hh
@@ -2,11 +2,13 @@
 #define JETSTREAM_REGISTRY_HH
 
 #include <functional>
+#include <cstdlib>
 #include <memory>
 #include <optional>
 #include <string>
+#include <type_traits>
+#include <utility>
 #include <vector>
-#include <unordered_map>
 
 #include "jetstream/provider.hh"
 #include "jetstream/runtime.hh"
@@ -81,29 +83,80 @@ class JETSTREAM_API Registry {
 
 }  // namespace Jetstream
 
+namespace Jetstream::detail {
+
+template<typename Callback>
+struct StaticRegistrar {
+    explicit StaticRegistrar(Callback callback) {
+        if (callback() != Result::SUCCESS) {
+            std::abort();
+        }
+    }
+};
+
+template<typename Callback>
+StaticRegistrar<std::decay_t<Callback>> MakeStaticRegistrar(Callback&& callback) {
+    return StaticRegistrar<std::decay_t<Callback>>(std::forward<Callback>(callback));
+}
+
+template<typename Impl>
+Result RegisterModuleType(DeviceType device,
+                          RuntimeType runtime,
+                          const ProviderType& provider) {
+    const auto module = std::make_shared<Impl>();
+
+    return ::Jetstream::Registry::RegisterModule(
+        module->type(),
+        device,
+        runtime,
+        provider,
+        [device, runtime, provider]() {
+            const auto impl = std::make_shared<Impl>();
+            const auto runtimeContext = std::static_pointer_cast<Runtime::Context>(impl);
+            const auto schedulerContext = std::static_pointer_cast<Scheduler::Context>(impl);
+            const auto context = std::make_shared<Module::Context>(runtimeContext, schedulerContext);
+            const auto stagedConfig = std::static_pointer_cast<Module::Config>(impl);
+            const auto candidateConfig = std::static_pointer_cast<Module::Config>(impl->candidate());
+            return std::make_shared<Module>(device,
+                                            runtime,
+                                            provider,
+                                            impl,
+                                            context,
+                                            stagedConfig,
+                                            candidateConfig);
+        }
+    );
+}
+
+template<typename Impl>
+Result RegisterBlockType() {
+    const auto block = std::make_shared<Impl>();
+
+    return ::Jetstream::Registry::RegisterBlock(
+        block->type(),
+        block->title(),
+        block->summary(),
+        block->description(),
+        []() {
+            const auto impl = std::make_shared<Impl>();
+            const auto stagedConfig = std::static_pointer_cast<Block::Config>(impl);
+            const auto candidateConfig = std::static_pointer_cast<Block::Config>(impl->candidate());
+            return std::make_shared<Block>(impl, stagedConfig, candidateConfig);
+        }
+    );
+}
+
+}  // namespace Jetstream::detail
+
 #define JST_DETAIL_CONCAT_IMPL(x, y) x##y
 #define JST_DETAIL_CONCAT(x, y) JST_DETAIL_CONCAT_IMPL(x, y)
 
 #define JST_DETAIL_REGISTER_MODULE(impl_type, device_val, runtime_val, provider_val, id) \
     namespace { \
-    static void JST_DETAIL_CONCAT(__jst_register_module_, id)() __attribute__((constructor)) __attribute__((used)); \
-    static void JST_DETAIL_CONCAT(__jst_register_module_, id)() { \
-        const auto& module = std::make_shared<impl_type>(); \
-        if (::Jetstream::Registry::RegisterModule( \
-            module->type(), device_val, runtime_val, provider_val, \
-            []() { \
-                const auto& impl = std::make_shared<impl_type>(); \
-                const auto& runtimeContext = std::static_pointer_cast<Runtime::Context>(impl); \
-                const auto& schedulerContext = std::static_pointer_cast<Scheduler::Context>(impl); \
-                const auto& context = std::make_shared<Module::Context>(runtimeContext, schedulerContext); \
-                const auto& stagedConfig = std::static_pointer_cast<Module::Config>(impl); \
-                const auto& candidateConfig = std::static_pointer_cast<Module::Config>(impl->candidate()); \
-                return std::make_shared<Module>(device_val, runtime_val, provider_val, impl, context, stagedConfig, candidateConfig); \
-            } \
-        ) != ::Jetstream::Result::SUCCESS) { \
-            std::abort(); \
-        } \
-    } \
+    [[maybe_unused]] const auto JST_DETAIL_CONCAT(__jst_register_module_, id) = \
+        ::Jetstream::detail::MakeStaticRegistrar([]() { \
+            return ::Jetstream::detail::RegisterModuleType<impl_type>(device_val, runtime_val, provider_val); \
+        }); \
     }
 
 #define JST_REGISTER_MODULE(impl_type, device_val, runtime_val, provider_val) \
@@ -111,21 +164,10 @@ class JETSTREAM_API Registry {
 
 #define JST_DETAIL_REGISTER_BLOCK(impl_type, id) \
     namespace { \
-    static void JST_DETAIL_CONCAT(__jst_register_block_, id)() __attribute__((constructor)) __attribute__((used)); \
-    static void JST_DETAIL_CONCAT(__jst_register_block_, id)() { \
-        const auto& block = std::make_shared<impl_type>(); \
-        if (::Jetstream::Registry::RegisterBlock( \
-            block->type(), block->title(), block->summary(), block->description(), \
-            []() { \
-                const auto& impl = std::make_shared<impl_type>(); \
-                const auto& stagedConfig = std::static_pointer_cast<Block::Config>(impl); \
-                const auto& candidateConfig = std::static_pointer_cast<Block::Config>(impl->candidate()); \
-                return std::make_shared<Block>(impl, stagedConfig, candidateConfig); \
-            } \
-        ) != ::Jetstream::Result::SUCCESS) { \
-            std::abort(); \
-        } \
-    } \
+    [[maybe_unused]] const auto JST_DETAIL_CONCAT(__jst_register_block_, id) = \
+        ::Jetstream::detail::MakeStaticRegistrar([]() { \
+            return ::Jetstream::detail::RegisterBlockType<impl_type>(); \
+        }); \
     }
 
 #define JST_REGISTER_BLOCK(impl_type) \
