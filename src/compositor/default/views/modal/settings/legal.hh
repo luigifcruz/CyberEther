087510e537345641ca427e2cd4a220515f9850b1
Luigi Cruz <github@luigi.ltd>
2026-05-30 18:00:58 -0700

    docs(legal): add OpenSSL acknowledgment

--- src/compositor/default/views/modal/settings/legal.hh
+++ src/compositor/default/views/modal/settings/legal.hh
@@ -105,6 +105,7 @@ struct LegalSettingsPanel : public Sakura::Component {
                    "- libusb - LGPL-2.1 License\n"
                    "- libqrencode - LGPL-2.1-or-later License\n"
                    "- zlib - zlib License\n"
+                   "- OpenSSL - Apache License 2.0\n"
                    "- Nanobench - MIT License\n"
                    "- Catch2 - Boost Software License\n"
                    "- Font Awesome - Font Awesome Free License\n"
