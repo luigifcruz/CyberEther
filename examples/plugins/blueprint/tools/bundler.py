3f2d659ac47fd911d14f048787660ba39b9282c2
luigifcruz <github@luigi.ltd>
2026-09-18 14:53:39 -0700

    chore(release): bump version to v1.11.0

--- examples/plugins/blueprint/tools/bundler.py
+++ examples/plugins/blueprint/tools/bundler.py
@@ -142,7 +142,7 @@ def main():
         dest="minimum_jetstream_version",
         required=True,
         type=parse_version,
-        help="Minimum CyberEther/Jetstream version, for example 1.10.2.",
+        help="Minimum CyberEther/Jetstream version, for example 1.11.0.",
     )
     parser.add_argument(
         "--target",
